e5735283ca56b1e4ac16ea461d77bf7536341c24
NicoleCoding <nicole251097@gmail.com>
2024-04-06 21:33:09 +0300

    Setup the basic structure for the game

---
+++ script.js
@@ -0,0 +1,16 @@
+const startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click", () => {
+    console.log("Start the game");
+});
+
+
+const game = (function () {
+
+})();
+
+
+
+function createPlayer(name, mark) {
+    
+}
