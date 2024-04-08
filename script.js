70b1ac27b48b28a18334681c6ad932bf79cafbe7
NicoleCoding <nicole251097@gmail.com>
2024-04-08 12:33:25 +0300

    Created module for the gameboard containing basic gameboard logic

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const startButton = document.querySelector("#start-button");
 
-startButton.addEventListener("click", () => {
-    console.log("Start the game");
-});
+// Gameboard module which contains the logic for getting and resetting a gameboard.
+const gameBoard = (function () {
+    const board = ["", "", "",
+                   "", "", "",
+                   "", "", ""];
 
+    function getBoard() {
+        return board;
 
+    }
+
+    function resetBoard() {
+        for(let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+
+    }
+
+    return {
+        getBoard, resetBoard
+    };
+
+})();
+
+               
+
+// Game module which contains all the game logic.
 const game = (function () {
 
 })();
 
 
-
+// Factory function for creating a player.
 function createPlayer(name, mark) {
+    return { name, mark };
     
 }
